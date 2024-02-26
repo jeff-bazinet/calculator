14946588f715cfab27461d1058fbe169c5b550e1
Jeff Bazinet <139579570+jeff-bazinet@users.noreply.github.com>
2024-02-26 08:40:00 -0500

    Add chooseOperation method

--- script.js
+++ script.js
@@ -15,29 +15,38 @@ let calculator = {
   appendNumber(number) {
     console.log(`appendNumber()`);
     // Do not allow more than one "." to be used.
-    if(number === '.' && this.currentOperand.includes('.')) return;
+    if (number === '.' && this.currentOperand.includes('.')) return;
     // Convert to string so that we can append text instead of js converting to a number
-    this.currentOperand =  this.currentOperand.toString() + number.toString();
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   },
   chooseOperation(operation) {
-
+    // Only execute this operation if there is a current operand.
+    if (this.currentOperand === '') return;
+    // If there is a current and previous operand, then execute the operation
+    if(this.previousOperand !== ''){
+        calculator.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
   },
   compute() {},
   updateDisplay() {
     currentOperandDisplayText.innerText = this.currentOperand;
+    previousOperandDisplayText.innerText = this.previousOperand;
   },
 };
 
 // Query Selectors for buttons and operations. Data attributes were used
 // so we don't mix our styling classes with javascript.
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operations]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const backspaceButton = document.querySelector('[data-backspace]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandDisplayText = document.querySelector([
-  'data-previous-operand',
-]);
+const previousOperandDisplayText = document.querySelector(
+  '[data-previous-operand]'
+);
 const currentOperandDisplayText = document.querySelector(
   '[data-current-operand]'
 );
@@ -55,13 +64,13 @@ numberButtons.forEach((button) => {
 
 // Operation button
 operationButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      calculator.chooseOperation(button.innerText);
-      calculator.updateDisplay();
-    });
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
   });
+});
 
 // All clear button
-allClearButton.addEventListener('click', ()=>{
-    calculator.allClear();
-})  
+allClearButton.addEventListener('click', () => {
+  calculator.allClear();
+});
